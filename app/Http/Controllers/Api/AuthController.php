9eb03938aebea15ae83586bbb1dd06c003a49206
gustonecrush <codeboxezz@gmail.com>
2023-01-21 22:35:32 +0700

    modified controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -31,39 +32,26 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json([
-            'data' => $user,
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
+        return $this->sendResponse($user, 'Successfully Register', 200, $token);
     }
 
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(
-                [
-                    'message' => 'Unauthorized',
-                ],
-                401
-            );
+            return $this->sendError('Unauthorized', [], 401);
         }
 
         $user = User::where('email', $request->email)->firstOrFail();
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json([
-            'message' => 'Login success',
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
+        return $this->sendResponse(null, 'Successfully Login', 200, $token);
     }
 
-    public function logout() 
+    public function logout()
     {
-        Auth::user()->tokens()->delete();
-        return response()->json([
-            'message' => 'Logout success',
-        ]);
+        Auth::user()
+            ->tokens()
+            ->delete();
+        return $this->sendResponse(null, 'Successfully Logout', 200, null);
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,43 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use function PHPUnit\Framework\isNull;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function sendResponse($result = null, $message, $code, $token = null,) {
+        $response = [
+            "success" => true,
+            "status_code" => $code,
+            "message" => $message,
+        ];
+
+        if($result !== null) {
+            $response["data"] = $result;
+        }
+
+        if($token !== null) {
+            $response["access_token"] = $token;
+            $response["token_type"] = "Bearer";
+        }
+
+        return response()->json($response, $code);
+    }
+
+    protected function sendError($error, $errorMessages = [], $code) {
+        $response = [
+            "success" => false,
+            "status_code" => $code,
+            "message" => $error,
+        ];
+
+        if(!empty($errorMessages)) {
+            $response["error"] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+
 }
